df90a6915116ec1ce548152e99fd284b8bbca7ae
Razor <rzrdeyafgh7@gmail.com>
2020-05-04 16:19:44 -0300

    prodcut page finished

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  cards: number[] = [0, 0, 0, 0, , 0, 0, 0, 0, 0, 0, , 0, 0, 0, 0, 0];
+  colCount = 5;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostListener("window:resize", ["$event"])
+  resizeWindow(event) {
+    const width = event.target.innerWidth;
+    if (width < 220) {
+      this.colCount = 1;
+    } else if (width < 480) {
+      this.colCount = 2;
+    } else if (width < 680) {
+      this.colCount = 3;
+    } else if (width < 768) {
+      this.colCount = 4;
+    } else {
+      this.colCount = 5;
+    }
   }
-
 }
